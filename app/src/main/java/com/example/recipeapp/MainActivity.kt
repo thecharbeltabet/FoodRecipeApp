a840cf40be424db5c66becfa87a018602bd212d2
Johnny Karam <JohnnyKaram@protonmail.com>
2021-06-02 04:32:26 +0300

    Navigation Changes

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -18,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.myNavHostFragment))
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
--- app/src/main/java/com/example/recipeapp/UserRecipesFragment.kt
+++ app/src/main/java/com/example/recipeapp/UserRecipesFragment.kt
@@ -6,22 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
-class MainFragment : Fragment() {
+class UserRecipesFragment : Fragment() {
     private lateinit var recipeViewModel: RecipeViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_user_recipes, container, false)
 
-        val query = FirebaseFirestore.getInstance().collection("recipes").orderBy("name")
+        /*val query = FirebaseFirestore.getInstance().collection("recipes").orderBy("name")
         val options = FirestoreRecyclerOptions.Builder<Recipe>().setQuery(query, Recipe::class.java).build()
         val adapter = RecipesListAdapter(options)
         val recyclerView = view.recyclerView
@@ -29,12 +28,10 @@ class MainFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         adapter.startListening()
 
-        recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
+        recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)*/
 
-        view.myRecipesButton.setOnClickListener{
-            findNavController().navigate(R.id.action_mainFragment_to_userRecipesFragment)
-        }
 
         return view
     }
+
 }
