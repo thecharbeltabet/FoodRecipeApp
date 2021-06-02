19fdc8b397469ceba98dc8308c588ab5f4ae5fec
Johnny Karam <JohnnyKaram@protonmail.com>
2021-06-02 05:14:29 +0300

    UI Fixes

--- app/src/main/java/com/example/recipeapp/UserRecipesFragment.kt
+++ app/src/main/java/com/example/recipeapp/UserRecipesFragment.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_main.view.*
+import kotlinx.android.synthetic.main.fragment_user_recipes.view.*
 
 class UserRecipesFragment : Fragment() {
     private lateinit var recipeViewModel: RecipeViewModel
@@ -20,15 +21,15 @@ class UserRecipesFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_user_recipes, container, false)
 
-        /*val query = FirebaseFirestore.getInstance().collection("recipes").orderBy("name")
+        val query = FirebaseFirestore.getInstance().collection("recipes").orderBy("name")
         val options = FirestoreRecyclerOptions.Builder<Recipe>().setQuery(query, Recipe::class.java).build()
         val adapter = RecipesListAdapter(options)
-        val recyclerView = view.recyclerView
+        val recyclerView = view.recyclerViewUser
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         adapter.startListening()
 
-        recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)*/
+        recipeViewModel = ViewModelProvider(this).get(RecipeViewModel::class.java)
 
 
         return view
